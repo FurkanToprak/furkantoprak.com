0ce110b9c437dde2d40167b1d408e38b7e91f4d1
Furkan Toprak <furkancemaltoprak@gmail.com>
2020-12-21 21:32:41 -0600

    Single-page routing.

--- src/App.tsx
+++ src/App.tsx
@@ -46,16 +46,16 @@ function App() {
                 <Route exact path={"/"}>
                   <Home />
                 </Route>
-                <Route path={"/about"}>
+                <Route exact path={"/about"}>
                   <About />
                 </Route>
-                <Route path={"/projects"}>
+                <Route exact path={"/projects"}>
                   <Projects />
                 </Route>
-                <Route path={"/contact"}>
+                <Route exact path={"/contact"}>
                   <Contact />
                 </Route>
-                <Route path={"/writings"}>
+                <Route exact path={"/writings"}>
                   <Writings />
                 </Route>
               </Switch>
