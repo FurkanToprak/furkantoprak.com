43ae8127d736c948bc4e4f0f09b2e6b384fb5023
Furkan Toprak <furkancemaltoprak@gmail.com>
2021-11-11 20:27:49 -0600

    work & research.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -59,45 +59,33 @@ function About() {
       >
         <MePicture />
       </div>
-      <div>
-        <h5 style={{ textAlign: "center" }}>what keeps me busy</h5>
-        I am an undergraduate student at Texas A&M University who is studying
-        Computer Science and Mathematics. I always strive to absorb as much
-        information as I can, in the hopes that it will one day benefit
-        humanity. My latest interests are human cognition, data engineering,
-        intelligent agents, distributed computation, game theory, evolutionary
-        dynamics, and socioeconomics.
-        <br />
-        <br />I have lately taken up writing and will try to publish my writings
-        on this website. You can visit the{" "}
-        <a href="/#/writings">
-          <u>writings</u>
-        </a>{" "}
-        tab for my essays and papers. If you have a project you would like me to
-        collaborate on, shoot me an email.
-      </div>
       <HR />
       <div>
-        <h5 style={{ textAlign: "center" }}>research & work</h5>
+        <h5 style={{ textAlign: "center" }}>work & research</h5>
+          <p>I'll be working at Google next year! I'll be located at the YouTube office in San Bruno.</p>
         <br />
+        <p>
+          This year, I am performing research on Explainability of Bias in Recommender Systems- under
+          James Caverlee at Texas A&M University. Previously, I have worked in a number of labs, primarily working with ML,
+          neuroevolution, data analysis, bacteria resistance, and viral genomics.
+        </p>
         <br />
-        This year, I am performing research on Explainability of Bias in Recommender Systems- under
-        James Caverlee at Texas A&M University. Previously, I have worked in a number of labs, primarily working with ML,
-        neuroevolution, data analysis, bacteria resistance, and viral genomics.
+        <p>
+          In the past, I have worked at Capital One as a Software Engineer Intern,
+          where I developed highly scalable machine learning microservices that analyzed financial contracts.
+          My work at Capital One is being used internally and in production.
+          Additionally, I have worked at SynchroGrid, developing software tools for
+          Electrical Engineers to protect power grids. My code has been shipped to
+          some of the largest power companies in the nation.
+        </p>
         <br />
-        <br />
-        In the past, I have worked at Capital One as a Software Engineer Intern,
-        where I developed highly scalable machine learning microservices that analyzed financial contracts.
-        My work at Capital One is being used internally and in production.
-        Additionally, I have worked at SynchroGrid, developing software tools for
-        Electrical Engineers to protect power grids. My code has been shipped to
-        some of the largest power companies in the nation.
-        I am a coauthor
-        in a publication! Head over to{" "}
-        <u>
-          <a href="/#/writings">writings</a>
-        </u>{" "}
-        to read it.
+        <p>
+          I am a coauthor in a publication! Head over to{" "}
+          <u>
+            <a href="/#/writings">writings</a>
+          </u>{" "}
+          to read it.
+        </p>
       </div>
       <HR />
       <div>
